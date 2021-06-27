0d194ce5e31252df6b327a81141a5e6c0ec0fca2
cagfcagf <cagfeijoo@gmail.com>
2021-06-27 19:25:16 -0400

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,4 @@
-<!-- Event snippet for Solicitar cotización conversion page
-In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. -->
-<script>
+
 function gtag_report_conversion(url) {
   var callback = function () {
     if (typeof(url) != 'undefined') {
@@ -13,4 +11,3 @@ function gtag_report_conversion(url) {
   });
   return false;
 }
-</script>
