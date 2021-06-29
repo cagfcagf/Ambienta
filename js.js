91bf83627c3dd7fd7143fed1aeb55c23ae852095
Cristian <cagfeijoo@gmail.com>
2021-06-29 15:42:06 -0400

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,4 @@
-<!-- Event snippet for Solicitar cotización conversion page
-In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. -->
+<!-- Event snippet for Solicitar cotización conversion page In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. -->
 <script>
 function gtag_report_conversion(url) {
   var callback = function () {
